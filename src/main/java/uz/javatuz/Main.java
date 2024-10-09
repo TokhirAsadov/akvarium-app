1090e427541569cb6cc5d38ff50afc1c3a7c12b8
Takhir <guvalakat1603@gmail.com>
2024-10-09 16:45:29 +0500

    run function is done;)

--- src/main/java/uz/javatuz/Main.java
+++ src/main/java/uz/javatuz/Main.java
@@ -41,7 +41,38 @@ class Fish implements Runnable {
 
     @Override
     public void run() {
+        Random random = new Random();
+        for (int day = 1; day <= lifetime; day++) {
+            System.out.println(name + " the " + gender + " fish is swimming. Day " + day);
 
+            // Tekshirish agar baliq boshqa baliq bilan uchrashsa
+            try {
+                Thread.sleep(1000); // Simulate a day passing
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Qarama qarshi jinsli baliqlarning uchrashib qolish ehtimolini tekshirish
+            aquariumLock.lock();
+            try {
+                Fish mate = findMate();
+                if (mate != null) {
+                    System.out.println(name + " met " + mate.getName() + ". They are having offspring!");
+                    int numOffspring = random.nextInt(3) + 1; // Yangi tug`ilgan baliqchalar sonini bilmaganim uchun shunday qildim! Random number of offspring between 1 and 3
+                    for (int i = 1; i <= numOffspring; i++) {
+                        String offspringGender = random.nextBoolean() ? "male" : "female"; // Yangi tug'ilgan har bir baliqning jinsi Random orqali aniqlanadi.
+                        String offspringName = "Offspring of " + name + " and " + mate.getName() + " " + i;
+                        int offspringLifetime = random.nextInt(20) + 10; // Lifetime between 10 and 29 days
+                        Fish offspring = new Fish(offspringName, offspringGender, offspringLifetime, aquarium, aquariumLock);
+                        aquarium.add(offspring);
+                        System.out.println("New fish born: " + offspringName + " (" + offspringGender + ")");
+                    }
+                }
+            } finally {
+                aquariumLock.unlock();
+            }
+        }
+        System.out.println(name + " the " + gender + " fish has passed away. Lifetime reached.");
     }
 
 
