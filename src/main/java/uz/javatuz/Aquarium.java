9b698bbdc690388bcc81223a9806fb9e38d4fef1
Takhir <guvalakat1603@gmail.com>
2024-10-12 18:43:22 +0500

    Aquarium

---
+++ src/main/java/uz/javatuz/Aquarium.java
@@ -0,0 +1,32 @@
+package uz.javatuz;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Aquarium {
+    private final int width;
+    private final int length;
+    private final List<Fish> fishes = new CopyOnWriteArrayList<>();  // Thread-safe list
+
+    public Aquarium(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public void addFish(Fish fish) {
+        fishes.add(fish);
+        new Thread(fish).start(); // Start a new thread for each fish
+    }
+
+    public List<Fish> getFishes() {
+        return fishes;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+}
